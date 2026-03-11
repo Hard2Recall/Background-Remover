fe75bf6561d6a1a28259c3bf2b605335b67ec556
Hard2Recall <simicandrija1306@gmail.com>
2026-03-11 17:32:35 +0100

    initial commit

---
+++ removerV1.py
@@ -0,0 +1,16 @@
+from withoutbg import WithoutBG
+
+# Initialize model once
+model = WithoutBG.opensource()
+
+img = "Screenshot_1.png" #instead of Screenshot_1.png type in your picture name and its extention
+
+# Process image
+result = model.remove_background(img)
+result.save('Output.png')
+
+# Process with progress callback
+def progress(value):
+    print(f"Progress: {value * 100:.1f}%")
+
+result = model.remove_background(img, progress_callback=progress)
